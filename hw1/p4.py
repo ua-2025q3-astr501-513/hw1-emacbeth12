ed70ecb98bf189d6ebc7021ff3528468eed2ac81
Emily Macbeth <emacbeth@Emilys-MacBook-Pro-7.local>
2025-09-10 10:00:21 -0700

    corrections to calling attributes

--- hw1/p4.py
+++ hw1/p4.py
@@ -60,12 +60,12 @@ class CoupledOscillators:
         
         # TODO: Store angular frequencies and eigenvectors
         # take square root so values are omega
-        Omega = np.sqrt(eigenvalues)
-        V = eigenvectors
+        self.Omega = np.sqrt(eigenvalues)
+        self.V = eigenvectors
 
         # TODO: Compute initial modal amplitudes M0 (normal mode decomposition)
         # move to mode space 
-        M0 = V.T @ X0
+        self.M0 = self.V.T @ X0
 
     def __call__(self, t):
         """Calculate the displacements of the oscillators at time t.
